c83e5c0c503587b3cc6b7259601a14aefe567b58
Turner Vink <turner@turnervink.com>
2017-05-08 16:28:45 -0700

    Remove authentication test code

--- app/app.js
+++ app/app.js
@@ -1,20 +1,5 @@
 var greenlistApp = angular.module("greenlistApp", ["ngRoute", "firebase"]);
 
 greenlistApp.config(["$routeProvider", function($routeProvider) {
-    $routeProvider
-        .when("/list", {
-            templateUrl: "views/html/shopping.html",
-            controller: "ShoppingListCtrl"
-        })
-        .when("/history", {
-            templateUrl: "views/html/history.html",
-            controller: "HistoryListCtrl"
-        })
-        .when("/reports", {
-            templateUrl: "views/html/report.html",
-            controller: "ReportCtrl"
-        })
-        .otherwise({
-            redirectTo: "/list"
-        });
+
 }])
