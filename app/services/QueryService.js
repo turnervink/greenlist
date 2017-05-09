3c29c7dea6a47cd74e3cdcd0b5fed6d6278e24db
Turner Vink <turner@turnervink.com>
2017-05-09 11:37:21 -0700

    Outline funtions for query service

--- app/services/QueryService.js
+++ app/services/QueryService.js
@@ -1,3 +1,17 @@
 greenlistApp.service("DatabaseQuery", ["DatabaseRef", function(DatabaseRef) {
 
+    // TODO (Steven) Log waste score for an item
+
+    // TODO (Turner) Add a new item
+
+    // TODO (Turner) Move item from history to shopping
+
+    // TODO (Turner) Move item from shopping to history
+
+    // TODO (Turner) Check if item is in history
+
+    // TODO (Steven) Update wasteDataStatus for an item
+
+    // TODO (Steven) Check wasteDataStatus for an item
+
 }]);
