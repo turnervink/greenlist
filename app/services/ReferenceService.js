c67881aa8d93a97ebe0a8ef5892599917999df67
novanaz <steevochang@gmail.com>
2017-05-09 10:41:47 -0700

    reference services setup
    complete all reference setup

--- app/services/ReferenceService.js
+++ app/services/ReferenceService.js
@@ -3,18 +3,22 @@ greenlistApp.service("DatabaseRef", ["UserInfo", function(UserInfo) {
     var database = firebase.database();
     var items = database.ref(UserInfo.getCurrentUser().uid + "/items");
     var shoppinglist = database.ref(UserInfo.getCurrentUser().uid + "/items")
-        .orderByChild("list")
-        .equalTo("shopping");
+        .orderByChild("list");
+    var overallAverage = database.ref(UserInfo.getCurrentUser().uid + "/overallAverage");
+    var wasteDataStatus = database.ref(UserInfo.getCurrentUser().uid + "/wasteDataStatus");
 
     function getRefToSpecificList(list) {
-        return database.ref(UserInfo.getCurrentUser().uid + "/items")
-            .orderByChild("list")
+        return shoppinglist
             .equalTo(list);
     }
 
     return {
-        items: items;
-        getRefToSpecificList: getRefToSpecificList;
+        items: items,
+        overallAverage: overallAverage,
+        getRefToSpecificList: getRefToSpecificList,
+        wasteDataStatus: wasteDataStatus,
+        database: database,
+
     }
 
 }]);
