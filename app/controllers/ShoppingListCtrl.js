6566f80600b7c6ed185d5c0551daaf8556fb4935
Turner Vink <turner@turnervink.com>
2017-05-09 12:16:07 -0700

    Create add item and set list functions

--- app/controllers/ShoppingListCtrl.js
+++ app/controllers/ShoppingListCtrl.js
@@ -1,7 +1,14 @@
 greenlistApp.controller("ShoppingListCtrl",
-    ["CurrentAuth", "$scope", "UserInfo", "DatabaseRef", "$firebaseObject",
-    function(CurrentAuth, $scope, UserInfo, DatabaseRef, $firebaseObject) {
+    ["CurrentAuth", "$scope", "UserInfo", "DatabaseRef", "DatabaseQuery", "$firebaseObject",
+    function(CurrentAuth, $scope, UserInfo, DatabaseRef, DatabaseQuery, $firebaseObject) {
 
     UserInfo.initUser(CurrentAuth.displayName, CurrentAuth.uid, CurrentAuth.photoURL);
-    
+
+        $scope.addItem = function() {
+            DatabaseQuery.addItem($scope.itemName);
+        }
+
+        $scope.updateList = function() {
+            DatabaseQuery.setItemList($scope.itemName, "history");
+        }
 }]);
--- app/services/QueryService.js
+++ app/services/QueryService.js
@@ -3,15 +3,53 @@ greenlistApp.service("DatabaseQuery", ["DatabaseRef", function(DatabaseRef) {
     // TODO (Steven) Log waste score for an item
 
     // TODO (Turner) Add a new item
+    function addItem(itemName) {
+        var newItem = {
+            list: "shopping",
+            name: itemName,
+            dataUpdated: false,
+            average: 0
+        }
+
+        console.log("Looking for " + newItem.name + " in history...");
+        var isInHistory;
+        DatabaseRef.items().once("value")
+            .then(function(data) {
+                console.log("Got data");
+                isInHistory = data.hasChild(newItem.name)
+                    && (data.child(newItem.name).val().list) == "history";
+
+                if (isInHistory) {
+                    console.log(newItem.name + " exists in history!");
+                    /* TODO Check if wasteDataStatus is true
+                       if true do not update and add to list
+                       if false update waste data, flip to true, and add to list
+                    */
+                } else {
+                    console.log(newItem.name + " is brand new!");
+                    // TODO Set wasteDataStatus to false
+                    DatabaseRef.items().child(newItem.name).set(newItem);
+                }
+            });
+    }
 
     // TODO (Turner) Move item from history to shopping
+    function setItemList(item, list) {
+        DatabaseRef.items()
+            .child(item)
+            .child("list")
+            .set(list);
 
-    // TODO (Turner) Move item from shopping to history
-
-    // TODO (Turner) Check if item is in history
+        // TODO update waste status if item was moved to shopping
+    }
 
     // TODO (Steven) Update wasteDataStatus for an item
 
     // TODO (Steven) Check wasteDataStatus for an item
+
+    return {
+        addItem: addItem,
+        setItemList: setItemList
+    }
 
 }]);
