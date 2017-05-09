0cdf7b3c44dc39f7640306ac1f3e75f790a60e79
Turner Vink <turner@turnervink.com>
2017-05-09 16:30:44 -0700

    Create function to check if item is historical

--- app/services/QueryService.js
+++ app/services/QueryService.js
@@ -10,7 +10,7 @@ greenlistApp.service("DatabaseQuery", ["DatabaseRef", function(DatabaseRef) {
 
         var push = DatabaseRef.wasteData(item).push();
         push.set(wasteScore);
-        updateWasteDataStatus(item.name, true);
+        updateWasteDataStatus(item, true);
     }
 
     // TODO (Turner) Add a new item
@@ -24,31 +24,48 @@ greenlistApp.service("DatabaseQuery", ["DatabaseRef", function(DatabaseRef) {
         };
 
         console.log("Looking for " + newItem.name + " in history...");
-        var isInHistory;
-        DatabaseRef.items().once("value")
-            .then(function(data) {
-                console.log("Got data");
-                isInHistory = data.hasChild(newItem.name)
-                    && (data.child(newItem.name).val().list) === "history";
+        // var isInHistory;
+        // DatabaseRef.items().once("value")
+        //     .then(function(data) {
+        //         console.log("Got data");
+        //         itemIsInHistory(newItem, function(historical) {
+        //             if (historical) {
+        //                 console.log(newItem.name + " exists in history!");
+        //                 checkWasteDataStatus(newItem, function(dataUpdated) {
+        //                     if (!dataUpdated) {
+        //                         // TODO Show modal and ask user for waste data
+        //                         updateWasteScore(newItem, prompt("Waste?"));
+        //                     } else {
+        //
+        //                     }
+        //                 });
+        //             } else {
+        //                 console.log(newItem.name + " is brand new!");
+        //                 DatabaseRef.items().child(newItem.name).set(newItem);
+        //             }
+        //         });
 
-                if (isInHistory) {
-                    console.log(newItem.name + " exists in history!");
-                    checkWasteDataStatus(newItem, function(dataUpdated) {
-                       if (!dataUpdated) {
-                           // TODO Show modal and ask user for waste data
-                           updateWasteScore(newItem, prompt("Waste?"));
-                       } else {
+                itemIsInHistory(newItem, function(historical) {
+                    if (historical === true) {
+                        console.log(newItem.name + " exists in history!");
 
-                       }
-                    });
-                } else {
-                    console.log(newItem.name + " is brand new!");
-                    DatabaseRef.items().child(newItem.name).set(newItem);
-                }
+                        checkWasteDataStatus(newItem, function(dataUpdated) {
+                            if (!dataUpdated) {
+                                // TODO Show modal and ask user for waste data
+                                updateWasteScore(newItem, prompt("Waste?"));
+                                setItemList(newItem, "shopping");
+                            } else {
+                                setItemList(newItem, "shopping");
+                            }
+                        });
+                    } else {
+                        console.log(newItem.name + " is brand new!");
+                        DatabaseRef.items().child(newItem.name).set(newItem);
+                    }
+                });
 
-                setItemList(newItem, "shopping");
                 // updateWasteDataStatus(newItem, false); // TODO Only do this after items have been checked and archived
-            });
+
     }
 
     // TODO (Turner) Move item from history to shopping
@@ -72,6 +89,17 @@ greenlistApp.service("DatabaseQuery", ["DatabaseRef", function(DatabaseRef) {
         check.once("value")
             .then(function(value){
                 callback(value.val());
+            });
+    }
+
+    function itemIsInHistory(item, callback) {
+        DatabaseRef.items().once("value")
+            .then(function(data) {
+                console.log("Got data");
+                var isInHistory = data.hasChild(item.name)
+                    && (data.child(item.name).val().list) === "history";
+
+                callback(isInHistory);
             });
     }
 
