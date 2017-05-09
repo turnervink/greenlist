2b20da0f7a2c8ef372a9c43992cd4f252720073c
novanaz <steevochang@gmail.com>
2017-05-08 18:07:20 -0700

    getreftospecificlist
    return function test

--- app/services/ReferenceService.js
+++ app/services/ReferenceService.js
@@ -1,3 +1,20 @@
 greenlistApp.service("DatabaseRef", ["UserInfo", function(UserInfo) {
 
+    var database = firebase.database();
+    var items = database.ref(UserInfo.getCurrentUser().uid + "/items");
+    var shoppinglist = database.ref(UserInfo.getCurrentUser().uid + "/items")
+        .orderByChild("list")
+        .equalTo("shopping");
+
+    function getRefToSpecificList(list) {
+        return database.ref(UserInfo.getCurrentUser().uid + "/items")
+            .orderByChild("list")
+            .equalTo(list);
+    }
+
+    return {
+        items: items;
+        getRefToSpecificList: getRefToSpecificList;
+    }
+
 }]);
