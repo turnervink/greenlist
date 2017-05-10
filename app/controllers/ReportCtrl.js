39b99d24deb34d8eba44a37d233310e0ba1eedc4
Andy Tang <andy_tanghao27@hotmail.com>
2017-05-10 15:12:18 -0700

    panel and page view ratio adjusted to fit

--- app/controllers/ReportCtrl.js
+++ app/controllers/ReportCtrl.js
@@ -2,6 +2,8 @@ greenlistApp.controller("ReportCtrl",
     ["CurrentAuth", "$scope", "UserInfo", "DatabaseRef", "$firebaseObject",
     function(CurrentAuth, $scope, UserInfo, DatabaseRef, $firebaseObject) {
 
+    	$scope.heading = 'Report';
+
     UserInfo.initUser(CurrentAuth.displayName, CurrentAuth.uid, CurrentAuth.photoURL);
 
 }]);
--- app/controllers/ShoppingListCtrl.js
+++ app/controllers/ShoppingListCtrl.js
@@ -2,6 +2,8 @@ greenlistApp.controller("ShoppingListCtrl",
     ["CurrentAuth", "$scope", "UserInfo", "DatabaseRef", "$firebaseObject",
     function(CurrentAuth, $scope, UserInfo, DatabaseRef, $firebaseObject) {
 
+    	$scope.heading = 'Shopping List';
+
     UserInfo.initUser(CurrentAuth.displayName, CurrentAuth.uid, CurrentAuth.photoURL);
     
 }]);
