bdf42ada53b544da2cd6cbae3ffea66019785d74
Andrew Mai <Luyen_mai@live.com>
2017-05-15 13:53:20 -0700

    About us page framework implemented

---
+++ public/app/controllers/AboutCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Currently dummy service to allow dynamic styling
+ * based on the loaded view.
+ */
+greenlistApp.controller("AboutCtrl", ["$scope", "UserInfo", function($scope, UserInfo) {
+
+}]);
