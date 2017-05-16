8a1ccdcd49a481cff67539e6fbf163895a65956a
Turner Vink <turner@turnervink.com>
2017-05-16 12:22:16 -0700

    Start of login flow fix?

--- public/app/controllers/AuthCtrl.js
+++ public/app/controllers/AuthCtrl.js
@@ -15,6 +15,7 @@ greenlistApp.controller("AuthCtrl", ["$scope", "$route", "$location", "UserInfo"
         auth.$signInWithRedirect("google").then(function(firebaseUser) {
             console.log("Signed in as " + firebaseUser.user.displayName);
             UserInfo.initUser(firebaseUser.user.displayName, firebaseUser.user.uid, firebaseUser.user.photoURL);
+            $location.url("list");
         }).catch(function(error) {
             console.error("Auth failed:", error);
         });
@@ -39,14 +40,14 @@ greenlistApp.controller("AuthCtrl", ["$scope", "$route", "$location", "UserInfo"
      * in the UserInfo service if the user is signed it, otherwise
      * does not do anything.
      */
-    auth.$onAuthStateChanged(
-        function(firebaseUser) {
-            if (firebaseUser) {
-                console.log("User is auth'd as " + firebaseUser.displayName);
-                UserInfo.initUser(firebaseUser.displayName, firebaseUser.uid, firebaseUser.photoURL, firebaseUser.email);
-                $location.url("list");
-            } else {
-                console.error("Could not auth user");
-            }
-        });
+    // auth.$onAuthStateChanged(
+    //     function(firebaseUser) {
+    //         if (firebaseUser) {
+    //             console.log("User is auth'd as " + firebaseUser.displayName);
+    //             UserInfo.initUser(firebaseUser.displayName, firebaseUser.uid, firebaseUser.photoURL, firebaseUser.email);
+    //             // $location.url("list");
+    //         } else {
+    //             console.error("Could not auth user");
+    //         }
+    //     });
 }]);
---
+++ public/app/controllers/LoginCtrl.js
@@ -0,0 +1,3 @@
+greenlistApp.controller("LoginCtrl", ["CurrentAuth, $scope", "$route", "$location", "UserInfo", "$firebaseAuth", function($scope, $route, $location, UserInfo, $firebaseAuth) {
+
+});
