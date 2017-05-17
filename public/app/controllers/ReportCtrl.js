98422df71913b5dca2d23f6dafb0cdd5ac14d6b7
Turner Vink <turner@turnervink.com>
2017-05-17 13:38:33 -0700

    Revert "Merge branch 'develop' into feature/report-styling"
    This reverts commit a1c4c0fe18e93a751eb68eb0b88913d2afc3b4de, reversing
    changes made to 9e742aff33d24f5459862b2d9c65dddc9eb9c49a.

--- public/app/controllers/ReportCtrl.js
+++ public/app/controllers/ReportCtrl.js
@@ -15,7 +15,7 @@ greenlistApp.controller("ReportCtrl",
         $scope.listBgImg = 'images/icons/cart-off.png';
         $scope.histBgImg = 'images/icons/hist-icon-off.png';
         $scope.reptBgImg = 'images/icons/reports-on.png';
-        $scope.menuHeight = '19vh';
+        $scope.menuHeight = '320px';
         $scope.menuMargin = '0';
 
 
@@ -82,12 +82,12 @@ greenlistApp.controller("ReportCtrl",
         $scope.dateRange = 'Last 2 Weeks';
 
         $scope.showDateMenu = function() {
-            $scope.menuHeight = '27.58vh';
-            $scope.menuMargin = '8.8vh';
+            $scope.menuHeight = '470px';
+            $scope.menuMargin = '150px';
         }
 
         $scope.hideDateMenu = function() {
-            $scope.menuHeight = '19vh';
+            $scope.menuHeight = '320px';
             $scope.menuMargin = '0';
         }
 }]);
