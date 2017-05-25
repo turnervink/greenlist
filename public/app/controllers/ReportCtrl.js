6aae6af55fc9ee37a6c14a2e018c582e6e81e8cf
novanaz <steevochang@gmail.com>
2017-05-24 17:01:47 -0700

    added modal for top 3 and bottom 3 items

--- public/app/controllers/ReportCtrl.js
+++ public/app/controllers/ReportCtrl.js
@@ -1,6 +1,6 @@
 greenlistApp.controller("ReportCtrl",
-    ["CurrentAuth", "$scope", "UserInfo", "DatabaseRef", "$firebaseObject", "$firebaseArray", "CalculationService", "DatabaseQuery",
-    function(CurrentAuth, $scope, UserInfo, DatabaseRef, $firebaseObject, $firebaseArray, CalculationService, DatabaseQuery) {
+    ["CurrentAuth", "$scope", "UserInfo", "DatabaseRef", "$firebaseObject", "$firebaseArray", "CalculationService", "DatabaseQuery","$uibModal",
+    function(CurrentAuth, $scope, UserInfo, DatabaseRef, $firebaseObject, $firebaseArray, CalculationService, DatabaseQuery, $uibModal) {
 
     	UserInfo.initUser(CurrentAuth.displayName, CurrentAuth.uid, CurrentAuth.photoURL, CurrentAuth.email);
 
@@ -128,12 +128,81 @@ greenlistApp.controller("ReportCtrl",
             $scope.menuMargin = '0';
         }
 
+        $scope.dataModal = function(item) {
+
+            // Get the needed data for graphing
+            DatabaseQuery.getChartData(item, function(dates, scores, recentDates, recentScores) {
+                console.log("Got dates and scores", dates, scores, recentDates, recentScores);
+
+                var modalInstance = $uibModal.open({
+                    templateUrl: 'views/partials/detailReportModal.html',
+                    windowClass: 'dataModal',
+
+                    // Controller for the modal
+                    controller: function($scope, $uibModalInstance) {
+                        $scope.food = item;
+
+                        // Chart variables
+                        $scope.labels = dates;
+                        $scope.chartData = scores;
+                        $scope.colors = ['#278518', '#278518', '#278518', '#278518'];
+                        $scope.options = {
+                            elements: {
+                                point: {}
+                            },
+                            scales: {
+                                yAxes: [
+                                    {
+                                        id: 'y-axis-1',
+                                        type: 'linear',
+                                        display: true,
+                                        position: 'left',
+                                        ticks: {
+                                            beginAtZero: true,
+                                            max: 100,
+                                            stepSize: 50,
+                                            fontSize: 16,
+                                            fontFamily: "Asap"
+                                        }
+                                    }
+                                ],
+                                xAxes: [
+                                    {
+                                        display: true,
+                                        ticks: {
+                                            maxTicksLimit: 5,
+                                            fontSize: 16,
+                                            minRotation: 45,
+                                            fontFamily: "Asap"
+                                        }
+                                    }
+                                ]
+                            },
+                            pan: {
+                                enabled: true,
+                                mode: "x"
+                            },
+                            zoom: {
+                                enabled: true,
+                                drag: false,
+                                mode: "x"
+                            }
+                        };
+
+                        // Recent chart variables
+                        $scope.recentLabels = recentDates;
+                        $scope.recentChartData = recentScores;
+                    }
+
+                });
+            });
+        }
+
 
         // Chart test code
         var item = {
             "name": "bread"
         };
-
 
 
 }]);
